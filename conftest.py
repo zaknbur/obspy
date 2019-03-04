97fcbaa6091fc59302bdc910f0578cb0c92ae3ee
derrick <djachambeador@gmail.com>
2019-03-03 21:48:47 -0800

    try to simulate report

--- conftest.py
+++ conftest.py
@@ -1,11 +1,142 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-Make sure the doctests run across numpy versions when using pytest.
+Obspy's testing configuration file.
 """
-import numpy as np
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+from future.builtins import *  # NOQA @UnusedWildImport
 
+import platform
+import sys
+import time
+
+import numpy as np
+import pytest
+
+from obspy.core.util.version import get_git_version
+from obspy.core.util import NETWORK_MODULES
+import obspy.core.util.testing as otest
+# from obspy.core.util.testing import _create_report, DEFAULT_TEST_SERVER, _send_report
+
+
+PSTATS_HELP = """
+Call "python -m pstats obspy.pstats" for an interactive profiling session.
+
+The following commands will produce the same output as shown above:
+  sort cumulative
+  stats obspy. 20
+
+Type "help" to see all available options.
+"""
+
+# Set legacy printing for numpy so the doctests work regardless of the numpy
+# version.
 try:
     np.set_printoptions(legacy='1.13')
 except TypeError:
     pass
+
+HOSTNAME = platform.node().split('.', 1)[0]
+
+
+@pytest.fixture(scope='session', autouse=True)
+def set_numpy_print_options():
+    """
+    Make sure the doctests print the same style of output across all numpy
+    versions.
+    """
+    try:
+        np.set_printoptions(legacy='1.13')
+    except (TypeError, AttributeError):
+        pass
+
+
+def pytest_addoption(parser):
+    parser.addoption('--obspy-version', action='store_true', default=False,
+                     help='print obspy version and exit')
+    parser.addoption('--network', action='store_true', default=False,
+                     help='test network modules', )
+    # reporting options
+    report = parser.getgroup('Reporting Options')
+    report.addoption('--report', action='store_true', default=False,
+                     help='automatically submit a test report')
+    report.addoption('--server', default=otest.DEFAULT_TEST_SERVER,
+                     help='report server (default is tests.obspy.org)')
+    report.addoption('--node', dest='hostname', default=HOSTNAME,
+                     help='nodename visible at the report server')
+    report.addoption('--log', default=None,
+                     help='append log file to test report')
+    report.addoption('--ci-url', default=None, dest="ci_url",
+                     help='URL to Continuous Integration job page.')
+    report.addoption('--pr-url', default=None,
+                     dest="pr_url", help='Github (Pull Request) URL.')
+    # other options
+    others = parser.getgroup('Additional Options')
+    others.addoption('--tutorial', action='store_true',
+                     help='add doctests in tutorial')
+    others.addoption('--no-formatting', action='store_true',
+                     help='skip code formatting test')
+    others.addoption('--keep-images', action='store_true',
+                     help='store images created during image comparison '
+                          'tests in subfolders of baseline images')
+    others.addoption('--keep-only-failed-images', action='store_true',
+                     help='when storing images created during testing, '
+                          'only store failed images and the corresponding '
+                          'diff images (but not images that passed the '
+                          'corresponding test).')
+
+
+def pytest_collection_modifyitems(config, items):
+    """ Preprocessor for collected tests. """
+    network_nodes = set(NETWORK_MODULES)
+    for item in items:
+        # get the obspy model test originates from (eg clients.arclink)
+        obspy_node = '.'.join(item.nodeid.split('/')[1:3])
+        # if test is a network test apply network marker
+        if obspy_node in network_nodes:
+            item.add_marker(pytest.mark.network)
+
+
+def pytest_configure(config):
+    # if obspy version just print it and exit
+    if config.getoption('--obspy-version'):
+        print(get_git_version())
+        sys.exit(0)
+    # If the all option is not set skip all network tests
+    if not config.getoption('--network'):
+        setattr(config.option, 'markexpr', 'not network')
+
+
+def pytest_sessionstart(session):
+    """ Add a results dict  to session object. """
+    # add results dict and start time to session object
+    session._results = dict()
+    session._starttime = time.time()
+
+
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item):
+    """
+    Hook to append each test result to pytest session.
+
+    See this stack overflow for the basic idea: https://goo.gl/hRCPSv
+    """
+    outcome = yield
+    result = outcome.get_result()
+    if result.when == 'call':
+        # ensure there is a monkeypatched results object here
+        item.session._results[result.nodeid] = result
+
+
+def pytest_html_results_summary(prefix, summary, postfix):
+    """ Called before adding the summary section to the report """
+    breakpoint()
+
+
+def pytest_sessionfinish(session, exitstatus):
+    """ Hook called when all tests runs finish. """
+    if session.config.getoption('--report'):
+        results, params = otest._create_report(session)
+        breakpoint()
+        otest._send_report(session, params)
--- obspy/core/util/testing.py
+++ obspy/core/util/testing.py
@@ -19,10 +19,13 @@ import glob
 import inspect
 import io
 import os
+import platform
 import re
 import shutil
+import time
 import unittest
 import warnings
+from collections import defaultdict, Counter
 from distutils.version import LooseVersion
 
 import numpy as np
@@ -30,6 +33,9 @@ from lxml import etree
 
 from obspy.core.util.base import NamedTemporaryFile, MATPLOTLIB_VERSION
 from obspy.core.util.misc import MatplotlibBackend
+
+# The default url to upload test resports.
+DEFAULT_TEST_SERVER = 'tests.obspy.org'
 
 # this dictionary contains the locations of checker routines that determine
 # whether the module's tests can be executed or not (e.g. because test server
@@ -1048,6 +1054,295 @@ def setup_context_testcase(test_case, cm):
     val = cm.__enter__()
     test_case.addCleanup(cm.__exit__, None, None, None)
     return val
+
+
+def _create_report(session):
+    """
+    If `server` is specified without URL scheme, 'https://' will be used as a
+    default.
+    """
+    # import additional libraries here to speed up normal tests
+    from future import standard_library
+    with standard_library.hooks():
+        import urllib.parse
+
+    from obspy.core.util.version import get_git_version
+    from xml.etree import ElementTree
+    from obspy.core.compatibility import urlparse
+    import requests
+    import obspy.core.util.base as obase
+    # from obspy.core.base import ALL_MODULES, NETWORK_MODULES
+    from xml.sax.saxutils import escape
+
+    def _read_log(path):
+        """ Try to read the system log, else return None. """
+        import codecs
+        try:
+            data = codecs.open(path, 'r', encoding='UTF-8').read()
+        except Exception:
+            print("Cannot open log file %s" % log)
+        else:
+            return escape(data)
+
+    def _get_module_name(nodeid):
+        """ Convert a pytest nodeid to an obspy model. """
+        return '.'.join(nodeid.split('/')[1:3]).replace('.tests', '')
+
+    def _get_tested_module_dict(results):
+        """ Return a dict of {tested_module: [test_results, ]} """
+        out = defaultdict(list)
+        for name, result in results.items():
+            out[_get_module_name(name)].append(result)
+        return out
+
+    def _split_node(nodeid):
+        """ split a node into test_module, test_class, test_name """
+        test_module_name = nodeid.split('::')[0].replace('/', '.')
+        nodes = nodeid.split('/')[-1].split('::')[1:]
+        if len(nodes) == 1:  # if this is a test outside of class
+            test_name = nodes[-1]
+            cls_name = ''
+        else:
+            test_name = nodes[-1]
+            cls_name = nodes[0]
+        return test_module_name.replace('.py', ''), cls_name, test_name
+
+    def _count_outcomes(ttrs):
+        """ Count the outcomes of test results. """
+        # map pytest outcomes to names expected in report
+        oc_map = dict(failed='failures', errored='errors', skipped='skipped',
+                      passed='passes')
+        count = Counter(oc_map[x.outcome] for x in ttrs)
+        count['tests'] = len(ttrs)
+        # init empty counts so counter can be used to update dict later on
+        for outcome in oc_map.values():
+            if outcome not in count:
+                count[outcome] = 0
+        count.pop('passes', None)
+        return count
+
+    def _dict2xml(result, doc=None):
+        """ Generate xml document to send to server. """
+        doc = doc or ElementTree.Element("report")
+        for key, value in result.items():
+            key = key.split('(')[0].strip()
+            if isinstance(value, dict):
+                child = ElementTree.SubElement(doc, key)
+                _dict2xml(value, child)
+            elif value is not None:
+                if isinstance(value, (str, native_str)):
+                    ElementTree.SubElement(doc, key).text = value
+                elif isinstance(value, (str, native_str)):
+                    ElementTree.SubElement(doc, key).text = str(value, 'utf-8')
+                else:
+                    ElementTree.SubElement(doc, key).text = str(value)
+            else:
+                ElementTree.SubElement(doc, key)
+        return ElementTree.tostring(doc)
+
+    def _get_module_info(module, ttrs):
+        """ Create a dict of results for a given module. """
+
+        out = {'installed': installed}
+        # TODO get modules that fail to import...
+        # add a failed-to-import test module to report with an error
+        # if module in import_failures:
+        #     result['obspy'][module]['timetaken'] = 0
+        #     result['obspy'][module]['tested'] = True
+        #     result['obspy'][module]['tests'] = 1
+        #     # can't say how many tests would have been in that suite so just
+        #     # leave 0
+        #     result['obspy'][module]['skipped'] = 0
+        #     result['obspy'][module]['failures'] = {}
+        #     result['obspy'][module]['errors'] = {
+        #         'f%s' % (errors): import_failures[module]}
+        #     tests += 1
+        #     errors += 1
+        #     continue
+        if module not in ttrs:
+            return out
+        test_results = ttrs[module]  # a list of test results for module
+        # count tests errors, runs, failures, skips
+        outcomes = _count_outcomes(test_results)
+        # test results
+        out['timetaken'] = sum(x.duration for x in test_results)
+        out['tested'] = True
+        out['tests'] = outcomes['tests']
+        out['skipped'] = outcomes['skipped']
+        # depending on module type either use failure (network related modules)
+        # or errors (all others)
+        out['errors'] = {}
+        out['failures'] = {}
+        count = 0
+        for result in test_results:
+            # TODO figure out how to tell if failure or error (errors have
+            # outcome == 'failed)
+            if result.outcome == 'failed':
+                out['failures']['f%d' % count] = result.longreprtext
+                count += 1
+        return out
+
+    def _get_dependency_versions():
+        """ Return a dict of versions of obspy dependencies. """
+        out = {}
+        for module in obase.DEPENDENCIES:
+            if module == "pep8-naming":
+                module_ = "pep8ext_naming"
+            else:
+                module_ = module
+            temp = module_.split('.')
+            try:
+                mod = __import__(module_,
+                                 fromlist=[native_str(temp[1:])])
+            except ImportError:
+                version_ = '---'
+            else:
+                try:
+                    version_ = mod.__version__
+                except AttributeError:
+                    version_ = '???'
+            out[module] = version_
+        return out
+
+    def _get_system_info(hostname):
+        """ Get system information. """
+        out = {}
+        for func in ['system', 'release', 'version', 'machine',
+                     'processor', 'python_version', 'python_implementation',
+                     'python_compiler', 'architecture']:
+            try:
+                temp = getattr(platform, func)()
+                if isinstance(temp, tuple):
+                    temp = temp[0]
+                out[func] = temp
+            except Exception:
+                out[func] = ''
+        # set node name to hostname if set
+        out['node'] = hostname
+        # post only the first part of the node name (only applies to MacOS X)
+        try:
+            out['node'] = result['platform']['node'].split('.')[0]
+        except Exception:
+            pass
+        return out
+
+    def _get_skipped_test_details(test_results):
+        """ Append message for each skipped test. """
+        out = []
+        for module in test_results:
+            for test_result in test_results[module]:
+                if test_result.outcome != 'skipped':
+                    continue
+                module = _get_module_name(test_result.nodeid)
+                test_mod, cls, test_name = _split_node(test_result.nodeid)
+                reason = test_result.longrepr[-1].replace('Skipped: ', '')
+                out.append((module, test_mod, cls, test_name, reason))
+        return out
+
+    def _get_slowest_tests(results):
+        """ Return a tuple of slowests tests and their runtimes. """
+        out = []
+        # get a dict of {test_node: duration}
+        test_durations = {i: v.duration for i, v in results.items()}
+        # get a list of test_nodes sorted by duration (fastest first)
+        nodes_by_time = sorted(test_durations, key=lambda x: test_durations[x])
+        for node in nodes_by_time[:-20:-1]:
+            time_str = "%0.3fs" % test_durations[node]
+            nodesplit = node.split('::')
+            test_name = nodesplit[-1]
+            other = '.'.join(nodesplit[:-1]).replace('/', '.')
+            test_id_str = "%s (%s)" % (test_name, other)
+            out.append((time_str, test_id_str))
+        return out
+
+    # get command line arg info used in the report
+    hostname = session.config.getoption('--node')
+    ci_url = session.config.getoption('--ci-url')
+    pr_url = session.config.getoption('--pr-url')
+    log = session.config.getoption('--log')
+    # get time taken for entire test suite to run
+    timestamp = time.time()
+    timetaken = timestamp - session._starttime
+    # get dict of {module: [test_result,]}
+    ttrs = _get_tested_module_dict(session._results)
+    import_failures = []
+    # get obspy version
+    try:
+        installed = get_git_version()
+    except Exception:
+        installed = ''
+    # init result dict and add basics to it
+    result = {'timestamp': int(timestamp), 'timetaken': timetaken, }
+    if log:
+        result['install_log'] = _read_log(log)
+    # add slowest tests to result
+    result['slowest_tests'] = _get_slowest_tests(session._results)
+
+    # add extra urls
+    if ci_url is not None:
+        result['ciurl'] = ci_url
+    if pr_url is not None:
+        result['prurl'] = pr_url
+    result['obspy'] = {'installed': installed}
+    # init a dict with ints for counting test outcomes
+    outcome_count = _count_outcomes(list(session._results.values()))
+    # iterate each module and add results to dict, also increment outcomes
+    for module in sorted(obase.ALL_MODULES):
+        info = _get_module_info(module, ttrs)
+        result['obspy'][module] = info
+    # add package versions to report
+    result['dependencies'] = _get_dependency_versions()
+    # add system / environment settings to report
+    result['platform'] = _get_system_info(hostname)
+    # update outcome counts
+    result.update(outcome_count)
+    # append info on skipped tests:
+    result['skipped_tests_details'] = _get_skipped_test_details(ttrs)
+    # generate params to send to sever
+    params = urllib.parse.urlencode({
+        'timestamp': timestamp,
+        'system': result['platform']['system'],
+        'python_version': result['platform']['python_version'],
+        'architecture': result['platform']['architecture'],
+        'tests': outcome_count['tests'],
+        'failures': outcome_count['failures'],
+        'errors': outcome_count['errors'],
+        'modules': len(ttrs) + len(import_failures),
+        'xml': _dict2xml(result)
+    })
+    return result, params
+
+
+def _send_report(session, params):
+    """ Send the test report to the server. """
+
+    from future import standard_library
+
+    from obspy.core.compatibility import urlparse
+    import requests
+
+
+
+    # get command line arg info used in the report
+    server = session.config.getoption('--server')
+
+    headers = {"Content-type": "application/x-www-form-urlencoded",
+               "Accept": "text/plain"}
+    url = server
+    if not urlparse(url).scheme:
+        url = "https://" + url
+    response = requests.post(url=url, headers=headers,
+                             data=params.encode('UTF-8'))
+    # get the response
+    if response.status_code == 200:
+        report_url = response.json().get('url', server)
+        print('Your test results have been reported and are available at: '
+              '{}\nThank you!'.format(report_url))
+    # handle errors
+    else:
+        print("Error: Could not send a test report to %s." % (server))
+        print(response.reason)
+
 
 
 if __name__ == '__main__':
